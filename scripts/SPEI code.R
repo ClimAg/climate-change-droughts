65a99318c016c746917afd106292bfcee6ba88f5
Evaklng <eva.kling3@gmail.com>
2022-06-30 11:45:28 +0200

    update

--- scripts/SPEI code.R
+++ scripts/SPEI code.R
@@ -1,4 +1,4 @@
-#SPI code#
+#SPEI code#
 
 # install packages
 install.packages("data.table")
@@ -9,7 +9,7 @@ library(data.table)
 #import data in data
 df <- read.table("./data/nasatea.csv", quote = "\"", sep=";", header=T, dec=".")
 
-# view first 6 rows of data
+# view first 5 rows of data
 head(df)
 # PARAMETER YEAR   JAN   FEB   MAR   APR   MAY   JUN   JUL   AUG   SEP   OCT   NOV   DEC   ANN
 # 1   T2M_MAX 1981 11.01 11.55 14.64 15.58 18.08 21.53 21.86 21.48 21.26 15.13 14.24 11.15 21.86
@@ -48,16 +48,67 @@ head(df1)
 # 5: 1981   MAY 5.35
 # 6: 1981   JUN 2.41
 
+#create maximal temperature vector
+df2<-subset(df, PARAMETER== "T2M_MAX", select=c(YEAR:DEC))
+df2<-melt(df2, id.var="YEAR", variable.name="MONTH")
+df2<-df2[order(df2$YEAR),]
+colnames(df2)[3]<-"Tmax"
+df2<-subset(df2, select = c(-YEAR, -MONTH))
+
+#view the 6 first rows
+head(df2)
+# Tmax
+# 1: 11.01
+# 2: 11.55
+# 3: 14.64
+# 4: 15.58
+# 5: 18.08
+# 6: 21.53
+
+#create minimal temperature vector
+df3<-subset(df, PARAMETER== "T2M_MIN", select=c(YEAR:DEC))
+df3<-melt(df3, id.var="YEAR", variable.name="MONTH")
+df3<-df3[order(df3$YEAR),]
+colnames(df3)[3]<-"Tmin"
+df3<-subset(df3, select = c(-YEAR, -MONTH))
+
+#view the 6 first rows
+head(df3)
+# Tmin
+# 1: -0.51
+# 2: -0.62
+# 3:  0.33
+# 4:  0.71
+# 5:  2.47
+# 6:  6.72
+
+# combine vectors to data frame
+df1<-cbind(df1,df2,df3)
+head(df1)
+# YEAR MONTH PRCP  Tmax  Tmin
+# 1: 1981   JAN 1.37 11.01 -0.51
+# 2: 1981   FEB 2.80 11.55 -0.62
+# 3: 1981   MAR 5.95 14.64  0.33
+# 4: 1981   APR 1.11 15.58  0.71
+# 5: 1981   MAY 5.35 18.08  2.47
+# 6: 1981   JUN 2.41 21.53  6.72
+
 #install package
 install.packages ("SPEI")
 #load package
 library(SPEI)
 
+#PET calculation
+df1$PET<-hargreaves(Tmin=df1$Tmin,Tmax=df1$Tmax, lat=52.164)
+
+#water balance calculation
+WBal<-df1$PRCP-df1$PET
+
 #create a list
-spi6<-spi(df1$PRCP,6)
+spei6<-spei(WBal,6)
 
 #display the values
-spi6
+spei6
 
 #create a graphic
-plot(spi6)
+plot.spei(spei6)
