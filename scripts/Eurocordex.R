d9c249ce908a946c3c536fbee1ece9fd756aedc0
Evaklng <eva.kling3@gmail.com>
2022-07-28 18:50:33 +0200

    new ;)

--- scripts/Eurocordex.R
+++ scripts/Eurocordex.R
@@ -1,72 +1,63 @@
 ### Euro Codrex ###
 
-# Source : https://climate4impact.eu/impactportal/general/index.jsp
-
 # install.packages("eurocordexr")
 library(eurocordexr)
-library(dplyr)
 library(tidyverse)
 library(lubridate)
-#library(ncdf4)
+# library(ncdf4)
+library(sf)
 
-##1971-1980
-pr7180 <- "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_197101-198012.nc"
-pr1 <- nc_grid_to_dt(
-  filename = pr7180,
-  variable = "pr"
-)
+# import functions
+source("functions/convert-units.R")
+source("functions/prprocess.R")
+source("functions/spi-spei.R")
 
-#choose a specific index cell##
-pr_df <- subset(pr1, icell==1)
+# ### find the closer cell to the Cork Airport station (Latitude: 51.84722 N, Longitude: -8.48611W, Station n° 3904, Height: 155 meters)
+# ncfile <- "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_197101-198012.nc"
+# # ncin <- nc_open(ncfile)
+# # View(ncin)
+# df_coord <- nc_grid_to_dt(filename = ncfile, variable = "lat")
+# df_coord$lon <- nc_grid_to_dt(filename = ncfile, variable = "lon")$lon
+#
+# df_coord$date <- NULL
+#
+# #add to the data frame another dataframe with the Cork Airport station latitude and longitude
+# df_coord <- rbind(data.frame(icell=0, lon=-8.5, lat=51.8), df_coord)
+#
+# df_coord_sf <- st_as_sf(df_coord, coords = c("lon", "lat"), crs = 4326)
+#
+# #crate a data frame corresponding to the Cork Airport station in a proper format
+# cork_airport <-subset(df_coord_sf, icell==0)
+#
+# #calculate the distance between the location's cell of the data set and the location of Cork Airport station
+# df_coord_sf$dist <- st_distance(df_coord_sf, cork_airport)
 
-pr_df$date <- as.Date(pr_df$date, format = "%Y-%m-%d")
+##extract the past precipitation data as the proper format
+pr1<-ec_process(datapath = "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_197101-198012.nc", indexcell = 69225, var="pr")
+pr2<-ec_process(datapath = "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_198101-199012.nc", indexcell = 69225, var="pr")
+pr3<-ec_process(datapath = "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_199101-200012.nc", indexcell = 69225, var="pr")
+pr4<-ec_process(datapath = "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_200101-200512.nc", indexcell = 69225, var="pr")
 
-pr_df$month <- month(pr_df$date)
+#create a data table for pr (1976-2005)
+pr_hist<-rbind(pr1, pr2, pr3, pr4)
 
-#change the date format : date(1990-12-06) in YEAR and MONTH columns
-x=data.frame(date = pr_df$date)
-
-x = x %>%
-  mutate(date = ymd(date)) %>%
-  mutate_at(vars(date), funs(year, month, day))
-
-x
-x<-subset(x, select = -c(day))
-x<-subset(x, select = -c(date))
-
-pr_df<-cbind(pr_df, x)
-
-##1981-1990
-pr8190 <- "./data/eurocordex/pastdata/pr_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_198101-199012.nc"
-pr2<- nc_grid_to_dt(
-  filename = pr8190,
-  variable = "pr"
-)
-
-#choose a specific index cell##
-pr_df2 <- subset(pr2, icell==1)
-
-#change the date format : date(1990-12-06) in YEAR and MONTH columns
-y=data.frame(date = pr_df2$date)
-
-y = y %>%
-  mutate(date = ymd(date)) %>%
-  mutate_at(vars(date), funs(year, month, day))
-
-y
-y<-subset(y, select = -c(day))
-y<-subset(y, select = -c(date))
-
-pr_df2<-cbind(pr_df2, y)
-
-#add a datatable one below an other
-pr_all<-rbind(pr_df, pr_df2)
+#select specific period : 1976-2005
+hist_data <- subset(pr_hist, year > 1975 & year < 2006)
 
 
+##extract the future precipitation data as the proper format
+pr5<-ec_process(datapath = "./data/eurocordex/futuredata/pr_EUR-11_NCC-NorESM1-M_rcp85_r1i1p1_DMI-HIRHAM5_v3_mon_204101-205012.nc", indexcell = 69225, var="pr")
+pr6<-ec_process(datapath = "./data/eurocordex/futuredata/pr_EUR-11_NCC-NorESM1-M_rcp85_r1i1p1_DMI-HIRHAM5_v3_mon_205101-206012.nc", indexcell = 69225, var="pr")
+pr7<-ec_process(datapath = "./data/eurocordex/futuredata/pr_EUR-11_NCC-NorESM1-M_rcp85_r1i1p1_DMI-HIRHAM5_v3_mon_206101-207012.nc", indexcell = 69225, var="pr")
 
-#tash<-nc_open("./data/eurocordex/tas_EUR-11_NCC-NorESM1-M_historical_r1i1p1_DMI-HIRHAM5_v3_mon_195101-196012.nc")
+#create a data table for pr (2041-2070)
+rcp85_data <-rbind (pr5, pr6, pr7)
 
-#t <- ncvar_get(tash, "tas")
+
+#callate spi
+#spi6
+
+
 
 # A package to make life easier working with daily netcdf files from the EURO-CORDEX RCMs. Relies on data.table to do the heavy data lifting.
 #
---
+++ scripts/SPI_Eurocordex.R
@@ -0,0 +1,88 @@
+###SPI from EURO-CORDEX###
+
+##SPI code Lattice Plot###
+
+# load libraries
+library(data.table)
+library(SPEI)
+library(lattice)
+library(latticeExtra)
+
+##using the Eurocordex script (hist_data and rcp85_data)
+
+#create a list
+spi12<-spi(hist_data$pr, 1)
+
+# #display the values
+spi12
+
+# create a time series from the SPI data
+spi12ts <- as.data.table(spi12$fitted)
+
+# merge the time series with the main data table
+hist_data1<-hist_data
+# # Convert numeric to month names
+# my_months_name <- month.name[hist_data1$month]
+# hist_data1$month<- my_months_name
+
+#add the spi values
+hist_data1$spi<- spi12ts
+
+#create a data frame with 3 columns (year, month, spi values)
+dfnew<-subset(hist_data1, select=c(month:spi))
+head(dfnew)
+# month year spi
+# 1:  January 1976  NA
+# 2: February 1976  NA
+# 3:    March 1976  NA
+# 4:    April 1976  NA
+# 5:      May 1976  NA
+# 6:     June 1976  NA
+
+
+#reshapedata
+dfnew<-dcast(hist_data1,
+             year~month,
+             value.var=c("spi"))
+head(dfnew)
+
+colnames(dfnew) <- c("year", month.name[hist_data1$month][1:12])
+
+#reset row names
+dfnew<-data.frame(dfnew)
+row.names(dfnew)<-dfnew$year
+dfnew[,1]<-NULL
+
+###create a Lattice Plot###
+
+# set plot resolution
+options(repr.plot.res = 200)
+
+# generate a matrix from the data
+m1 <- as.matrix(dfnew)
+
+# view the matrix
+m1
+
+#create spi color palette
+palette_spi <- palette(c("red", "orange", "yellow", "white", "#D2B4DE", "#8E44AD", "#4A235A"))
+
+#create a theme
+myTheme <- modifyList(custom.theme(region=palette_spi),
+                      list(
+                        panel.background=list(col="black")))
+
+#break color key
+breaks <- c(-4, -2, -1.5, -1, 1, 1.5, 2, 4)
+
+# plot the matrix
+levelplot(
+  m1,
+  par.settings=myTheme,
+  col.regions = palette_spi,
+  at = breaks,
+  # xlab ="Year",
+  xlab=list(label = "Year", cex=1.5),
+  ylab=list(label="Month", cex=1.5),
+  main=list(label="Past data (1976-2005) SPI1",cex=2)
+)
